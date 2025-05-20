8ee43f22a40a8c1fcc54113f38c18575cac65658
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-05-20 13:25:29 +0300

    Test Plan Report added

--- backlog.py
+++ backlog.py
@@ -68,6 +68,10 @@ def low_level_design_report():
     st.title("492 - Low Level Design Report")
     display_pdf("PostdisasterCommunication-LowLevelDesignReport.pdf")
 
+def test_plan_report():
+    st.title("492 - Test Plan Report")
+    display_pdf("PostdisasterCommunication-TestPlanReport.pdf")
+
 def backlog():
     with open('backlog.json', 'r') as file:
         data = json.load(file)
@@ -79,7 +83,7 @@ st.set_page_config(page_title="Project Management App", layout="wide", initial_s
 
 st.sidebar.title("Menu")
 page = st.sidebar.radio("Select a Page", 
-                         ["Home", "491 - Project Proposal", "491 - Project Specifications Report", "491 - Project Analysis Report", "491 - High Level Design Report", "492 - Low Level Design Report", "Backlog"])
+                         ["Home", "491 - Project Proposal", "491 - Project Specifications Report", "491 - Project Analysis Report", "491 - High Level Design Report", "492 - Low Level Design Report", "Test Plan Report", "Backlog"])
 
 if page == "Home":
     home()
@@ -93,5 +97,7 @@ elif page == "491 - High Level Design Report":
     high_level_design_report()
 elif page == "492 - Low Level Design Report":
     low_level_design_report()
+elif page == "492 - Test Plan Report":
+    test_plan_report()
 elif page == "Backlog":
     backlog()
